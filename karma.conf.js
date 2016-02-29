8ae9638753d6813d70e14cc360e8e037dd4286ff
Prabhat Kumar <prabhat.genome@gmail.com>
2016-02-29 05:41:23 +0530

    Initial commit
    --/The Heart of Test System/-- of "Iamprabhat®".

--- karma.conf.js
+++ karma.conf.js
@@ -1,1 +1,23 @@
 #!/usr/bin/env node
+
+
+/*!
+ * Iamprabhat® [v1.0.0]
+ * ______________________________________________________________
+ * karma, https://github.com/karma-runner/karma
+ * The Spectacular Test Runner for JavaScript.
+ * ______________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * ______________________________________________________________
+ * @date      : 29-Feb-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * ______________________________________________________________
+ *
+ * --/The Heart of Karma Test Runner System/-- of "Iamprabhat®".
+ * ______________________________________________________________
+ */

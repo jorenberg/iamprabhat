c385d434597c9e7864569f8a83d720537aab8045
Prabhat Kumar <prabhat.genome@gmail.com>
2016-03-01 13:46:39 +0530

    Initial commit
    --/Directives of MVW System/--

---
+++ app/script/directives/header.js
@@ -0,0 +1,20 @@
+/*!
+ * Iamprabhat® [v1.0.0]
+ * ______________________________________________________________
+ * AngularJS — Superheroic JavaScript MVW Framework.
+ * https://angularjs.org/.
+ * ______________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * ______________________________________________________________
+ * @date      : 29-Feb-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * ______________________________________________________________
+ *
+ * --/The Heart of MVW System/-- of "Iamprabhat®".
+ * ______________________________________________________________
+ */

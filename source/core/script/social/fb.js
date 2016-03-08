1a16f86a636a22fb7ac5039b17d3e518bf9570b6
Prabhat Kumar <prabhat.genome@gmail.com>
2016-03-08 10:35:40 +0530

    Initial commit
    The AMD JavaScript - for defining "FaceBook" configurations.
    Help: https://github.com/iamprabhat/iamprabhat/wiki

---
+++ source/core/script/social/fb.js
@@ -0,0 +1,20 @@
+/*!
+ * Iamprabhat® [v1.0.0]
+ * _______________________________________________________________
+ * AXIX®, https://github.com/AXIX
+ * The AMD JavaScript - for defining "FaceBook" configurations.
+ * _______________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * _______________________________________________________________
+ * @date      : 06-Mar-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * _______________________________________________________________
+ *
+ * --/The Heart of AMD System/-- of "Iamprabhat®".
+ * _______________________________________________________________
+ */

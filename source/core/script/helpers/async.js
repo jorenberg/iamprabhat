d206131a811f7967e152f378a6b78caca3e44f8f
Prabhat Kumar <prabhat.genome@gmail.com>
2016-03-08 16:34:03 +0530

    Initial commit
    async: An AMD plugin for asynchronously dependency loading.
    Help: https://github.com/millermedeiros/requirejs-plugins

---
+++ source/core/script/helpers/async.js
@@ -0,0 +1,18 @@
+/*!
+ * ~~~~~~~~~~~~
+ * async®     : An AMD plugin for asynchronously dependency loading.
+ * _________________________________________________________________
+ * @copyright : 2014, Miller Medeiros. All Rights Reserved.
+ * @help      : https://github.com/millermedeiros/requirejs-plugins
+ * @require   : requirejs
+ * @license   : MIT license.
+ * _________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * _________________________________________________________________
+ *
+ * --/The Heart of AMD System/-- of "Iamprabhat®".
+ * _________________________________________________________________
+ */

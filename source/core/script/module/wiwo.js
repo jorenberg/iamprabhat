c048d50e5548641ad1ba25b18063c5e9ea39f68d
Prabhat Kumar <prabhat.genome@gmail.com>
2016-03-08 10:04:01 +0530

    Initial commit
    wiwo.js, JavaScript i.e., transcompiles from CoffeeScript -> wiwo.coffee
    Help: https://github.com/iamprabhat/iamprabhat/wiki

---
+++ source/core/script/module/wiwo.js
@@ -0,0 +1,20 @@
+/*!
+ * Iamprabhat® [v1.0.0]
+ * _______________________________________________________________________
+ * AXIX®, https://github.com/AXIX
+ * The AMD JavaScript - for defining "What I’m Working On" configurations.
+ * _______________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * _______________________________________________________________________
+ * @date      : 06-Mar-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * _______________________________________________________________________
+ *
+ * --/The Heart of AMD System/-- of "Iamprabhat®".
+ * _______________________________________________________________________
+ */

f8afb57157621f51ea49e28f982ad682a208402f
Prabhat Kumar <prabhat.genome@gmail.com>
2016-03-08 17:04:04 +0530

    Initial commit
    util.js: A JavaScript plugin for Keyboard Input Utility.
    Help: https://github.com/iamprabhat/iamprabhat/kbdx

---
+++ source/core/script/helpers/util.js
@@ -0,0 +1,17 @@
+/*!
+ * ~~~~~~~~~~~~
+ * util.js®   : A JavaScript plugin for Keyboard Input Utility.
+ * ____________________________________________________________________
+ * @help      : https://github.com/iamprabhat/iamprabhat/kbdx
+ * @require   : requirejs
+ * @license   : MIT license.
+ * ____________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * ____________________________________________________________________
+ *
+ * --/The Heart of AMD System/-- of "Iamprabhat®".
+ * ____________________________________________________________________
+ */

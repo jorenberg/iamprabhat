a9a18febe63b3fc761feff2052349a34e3d92277
Prabhat Kumar <prabhat.genome@gmail.com>
2016-03-08 16:55:12 +0530

    Initial commit
    namespace.js: A RequireJS plugin for collecting modules into a namespace.
    Help: https://github.com/atesgoral/requirejs-namespace-plugin

---
+++ source/core/script/helpers/namespace.js
@@ -0,0 +1,18 @@
+/*!
+ * ~~~~~~~~~~~~
+ * namespace® : An AMD plugin for Namespace.
+ * ____________________________________________________________________
+ * @copyright : 2013, Ates Goral. All Rights Reserved.
+ * @help      : https://github.com/atesgoral/requirejs-namespace-plugin
+ * @require   : requirejs
+ * @license   : MIT license.
+ * ____________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * ____________________________________________________________________
+ *
+ * --/The Heart of AMD System/-- of "Iamprabhat®".
+ * ____________________________________________________________________
+ */

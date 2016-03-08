838a5704800c7e41cae8e174993c3f82cca69bcb
Prabhat Kumar <prabhat.genome@gmail.com>
2016-03-08 14:49:52 +0530

    Initial commit
    i18n: An AMD loader plugin for loading internationalization/localization string resources.
    Load string bundles used in internationalization (i18n) that are made up of separate country/language/locale-specific bundles.
    Help: https://github.com/requirejs/i18n

---
+++ source/core/script/plugin/amd/i18n.js
@@ -0,0 +1,19 @@
+/*!
+ * ~~~~~~~~~~~~
+ * i18n®      : An AMD loader plugin for loading
+ *              internationalization/localization string resources.
+ * ________________________________________________________________
+ * @copyright : 2010-2012, The Dojo Foundation All Rights Reserved.
+ * @help      : http://github.com/requirejs/i18n
+ * @require   : requirejs
+ * @license   : MIT or new BSD license.
+ * ________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * ________________________________________________________________
+ *
+ * --/The Heart of AMD System/-- of "Iamprabhat®".
+ * ________________________________________________________________
+ */

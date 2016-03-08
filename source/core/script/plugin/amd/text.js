37096c70aae7ac48c681282ce6319299ff017928
Prabhat Kumar <prabhat.genome@gmail.com>
2016-03-08 14:55:49 +0530

    Initial commit
    text: Load text files and treat them as dependencies.
    Great for loading templates.
    The text strings can be inlined in an optimized build when the optimizer is used.
    Help: http://github.com/requirejs/text

---
+++ source/core/script/plugin/amd/text.js
@@ -0,0 +1,18 @@
+/*!
+ * ~~~~~~~~~~~~
+ * text®      : An AMD loader plugin for loading text resources.
+ * ________________________________________________________________
+ * @copyright : 2010-2014, The Dojo Foundation All Rights Reserved.
+ * @help      : http://github.com/requirejs/text
+ * @require   : requirejs
+ * @license   : MIT or new BSD license.
+ * ________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * ________________________________________________________________
+ *
+ * --/The Heart of AMD System/-- of "Iamprabhat®".
+ * ________________________________________________________________
+ */

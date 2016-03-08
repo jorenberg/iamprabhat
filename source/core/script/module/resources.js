08d07d43aa1cace369bcc22319b706c827e7e515
Prabhat Kumar <prabhat.genome@gmail.com>
2016-03-08 09:56:17 +0530

    Initial commit
    resources.js, JavaScript i.e., transcompiles from CoffeeScript -> resources.coffee
    Help: https://github.com/iamprabhat/iamprabhat/wiki

---
+++ source/core/script/module/resources.js
@@ -0,0 +1,20 @@
+/*!
+ * Iamprabhat® [v1.0.0]
+ * ________________________________________________________________
+ * AXIX®, https://github.com/AXIX
+ * The AMD JavaScript - for defining "resources" configurations.
+ * ________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * ________________________________________________________________
+ * @date      : 06-Mar-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * ________________________________________________________________
+ *
+ * --/The Heart of AMD System/-- of "Iamprabhat®".
+ * ________________________________________________________________
+ */

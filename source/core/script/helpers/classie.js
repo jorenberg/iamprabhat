108b11fc783c2e540a14129477adef4f1f41dea1
Prabhat Kumar <prabhat.genome@gmail.com>
2016-03-08 17:18:47 +0530

    Initial commit
    classie: class helper functions.
    Help: https://github.com/ded/bonzo

---
+++ source/core/script/helpers/classie.js
@@ -0,0 +1,16 @@
+/*!
+ * ~~~~~~~~~~~~
+ * classie®   : class helper functions.
+ * ____________________________________________________________________
+ * @help      : https://github.com/ded/bonzo
+ * @license   : MIT license.
+ * ____________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * ____________________________________________________________________
+ *
+ * --/The Heart of AMD System/-- of "Iamprabhat®".
+ * ____________________________________________________________________
+ */

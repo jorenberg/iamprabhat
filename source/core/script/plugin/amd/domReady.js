c7a7f7f03de35ab4c1a81b29bba58dae3641490f
Prabhat Kumar <prabhat.genome@gmail.com>
2016-03-08 14:41:29 +0530

    Initial commit
    domReady: Wait for the DOM is ready. Useful for pausing execution of top level application logic until the DOM is ready for querying/modification.
    Help: https://github.com/requirejs/domReady

---
+++ source/core/script/plugin/amd/domReady.js
@@ -0,0 +1,18 @@
+/*!
+ * ~~~~~~~~~~~~
+ * domReady®  : An AMD loader plugin for detecting DOM ready.
+ * ________________________________________________________________
+ * @copyright : 2010-2012, The Dojo Foundation All Rights Reserved.
+ * @help      : http://github.com/requirejs/domReady
+ * @require   : requirejs
+ * @license   : MIT or new BSD license.
+ * ________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * ________________________________________________________________
+ *
+ * --/The Heart of AMD System/-- of "Iamprabhat®".
+ * ________________________________________________________________
+ */

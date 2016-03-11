c9abc84f4c712422548230c29037df51bd474d7c
Prabhat Kumar <prabhat.genome@gmail.com>
2016-03-11 16:20:22 +0530

    Initial commit
    main.js, JavaScript i.e., transcompiles from CoffeeScript -> main.coffee
    Help: https://github.com/iamprabhat/iamprabhat/wiki

---
+++ source/core/script/main.js
@@ -0,0 +1,20 @@
+/*!
+ * Iamprabhat® [v0.0.2]
+ * ______________________________________________________________
+ * AXIX®, https://github.com/AXIX
+ * The Non-AMD JavaScript - for defining "main" configurations.
+ * ______________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * ______________________________________________________________
+ * @date      : 11-Mar-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * ______________________________________________________________
+ *
+ * --/The Heart of Non-AMD System/-- of "Iamprabhat®".
+ * ______________________________________________________________
+ */
